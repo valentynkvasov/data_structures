334fb260a71d05ebf6e0c7e58a95039bb9e6db6a
valentynkvasov <valentynkvasov@gmail.com>
2022-03-12 18:23:48 +0200

    Add Segment Tree Lazy propagation

--- src/java/com/structure/segment_tree/LazySegmentTree.java
+++ src/java/com/structure/segment_tree/LazySegmentTree.java
@@ -1,13 +1,17 @@
 package com.structure.segment_tree;
 
-public class SegmentTree {
+public class LazySegmentTree {
 
     private final int[] arr;
     private final int[] st;
+    private final int[] lazy;
 
-    public SegmentTree(int[] arr) {
+    public LazySegmentTree(int[] arr) {
         this.arr = arr;
-        this.st = new int[getNextPowerOfTwo(arr.length) * 2 - 1];
+
+        int treeSize = getNextPowerOfTwo(arr.length);
+        this.lazy = new int[treeSize * 2 - 1];
+        this.st = new int[treeSize * 2 - 1];
     }
 
     public void buildSegmentTree() {
@@ -37,9 +41,20 @@ public class SegmentTree {
         if (qhigh < low || high < qlow) { //no overlap
             return 0;
         }
+
+        if(lazy[index] != 0) {
+            st[index] += lazy[index];
+            if(low != high) {
+                lazy[2 * index + 1] += lazy[index];
+                lazy[2 * index + 2] += lazy[index];
+            }
+            lazy[index] = 0;
+        }
+
         if (qlow <= low && high <= qhigh) { //full overlap
             return st[index];
         }
+
         int mid = getMid(low, high);
         return getRangeQuery(qlow, qhigh, low, mid, 2 * index + 1)
                 + getRangeQuery(qlow, qhigh, mid + 1, high, 2 * index + 2);
@@ -47,23 +62,36 @@ public class SegmentTree {
 
     public void updateRangeQuery(int index, int val) {
         arr[index] = val;
-        updateRangeQuery(0, arr.length - 1, 0, index);
+        updateRangeQuery(index, index, 0, arr.length - 1, 0, val);
     }
 
-    private void updateRangeQuery(int low, int high, int segmentIndex, int index) {
-        if (low == high) {
-            st[segmentIndex] = arr[low];
+    private void updateRangeQuery(int qlow, int qhigh, int low, int high, int index, int val) {
+        if(lazy[index] != 0) {
+            st[index] += lazy[index];
+            if(low != high) {
+                lazy[2 * index + 1] += lazy[index];
+                lazy[2 * index + 2] += lazy[index];
+            }
+            lazy[index] = 0;
+        }
+
+        if (low > high || low > qhigh || high < qlow)
+            return;                                             // out of range. escape.
+
+        if (qlow <= low && high <= qhigh) {
+            st[index] = val;
+
+            if(low != high) {
+                lazy[2 * index + 1] += val;
+                lazy[2 * index + 2] += val;
+            }
             return;
         }
         int mid = getMid(low, high);
+        updateRangeQuery(qlow, qhigh, low, mid, 2 * index + 1, val);
+        updateRangeQuery(qlow, qhigh, mid + 1, high, 2 * index + 2, val);
 
-        if (index <= mid) {
-            updateRangeQuery(low, mid, 2 * segmentIndex + 1, index);
-        } else {
-            updateRangeQuery(mid + 1, high, 2 * segmentIndex + 2, index);
-        }
-
-        st[segmentIndex] = st[2 * segmentIndex + 1] + st[2 * segmentIndex + 2];
+        st[index] = st[2 * index + 1] + st[2 * index + 2];
     }
 
     private int getNextPowerOfTwo(int len) {
@@ -85,7 +113,7 @@ public class SegmentTree {
         int[] arr = {1, 2, 3, 4, -3, 2, -10, 3, 4};
         //prefix sum 1  3  6  10  7  9  -1   2  6
         //indexes    0  1  2  3  4   5   6   7  8
-        SegmentTree tree = new SegmentTree(arr);
+        LazySegmentTree tree = new LazySegmentTree(arr);
 
         tree.buildSegmentTree();
 
